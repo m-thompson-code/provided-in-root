b8f7dc8a55748798b8feb91a9eb0557a175cd376
Mark Thompson <mthompson@bitovi.com>
2022-07-26 16:31:41 -0700

    provided in root only

--- src/app/services/ascii.service.ts
+++ src/app/services/ascii.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class AsciiService {
   injectArt(): void {
     const img = document.getElementsByTagName('img')[0]!;
