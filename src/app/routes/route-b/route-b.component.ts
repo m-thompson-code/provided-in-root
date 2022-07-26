2010b7cc558228f0b41d09deb553a04426feabca
Mark Thompson <mthompson@bitovi.com>
2022-07-26 16:28:53 -0700

    Just service

--- src/app/routes/route-b/route-b.component.ts
+++ src/app/routes/route-b/route-b.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-import { AsciiService } from '../../services/ascii.service';
 
 @Component({
   selector: 'app-route-b',
   templateUrl: './route-b.component.html',
   styleUrls: ['./route-b.component.scss']
 })
-export class RouteBComponent {
-  constructor(private readonly asciiService: AsciiService) { }
-
-  ngOnInit(): void {
-    this.asciiService.injectArt();
-  }
-}
+export class RouteBComponent {}
--- src/app/services/ascii.service.ts
+++ src/app/services/ascii.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class AsciiService {
   injectArt(): void {
     const img = document.getElementsByTagName('img')[0]!;
